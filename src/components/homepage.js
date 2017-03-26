d62063b5c956b8b38778455a06907892ada12801
Aidan Baack <aebaack@gmail.com>
2017-03-26 11:31:39 -0600

    Add React

---
+++ src/components/homepage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles/homepage.css';
+
+class Homepage extends Component {
+  render() {
+    return (
+      <div>Undertone</div>
+    );
+  }
+}
+
+export default Homepage;
