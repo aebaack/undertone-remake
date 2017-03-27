ee3322d56a5b2bcbc2aea13bed854769a27afd89
Aidan Baack <aebaack@gmail.com>
2017-03-27 16:04:16 -0600

    Format the list of poems

--- src/components/PoemList.js
+++ src/components/PoemList.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { Component } from 'react';
+import '../styles/poemlist.css';
 
 class PoemList extends Component {
   constructor(props) {
@@ -7,18 +8,32 @@ class PoemList extends Component {
     this.state = {
       poems: []
     }
+
+    this.formatPoemList = this.formatPoemList.bind(this);
   }
 
   componentWillMount() {
     axios.get(`https://poetdb.herokuapp.com/author/${this.props.match.params.poet}`)
       .then(poems => this.setState({ poems: poems.data }))
       .catch(err => this.setState({ poems: [] }))
+    // this.setState({ poems: [{title: 'Poem 1'}, {title: 'Poem 2'}] });
+  }
+
+  formatPoemList() {
+    return this.state.poems
+      .map(poem => (
+        <li className="poem-list-li">
+          <div className="poem-info-left">{poem.title}</div>
+          <i className="material-icons poem-info-right">keyboard_arrow_right</i>
+        </li>
+      ));
   }
 
   render() {
     return (
-      <div>
-        {this.state.poems.map(poem => <p>{poem.title}</p>)}
+      <div className="poem-list">
+        <h1 className="poet-name">{this.props.match.params.poet}</h1>
+        <ul className="poem-list-ul">{this.formatPoemList()}</ul>
       </div>
     );
   }
