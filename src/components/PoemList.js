536cdc17e31fb64ee2616c095ca3614045727084
Aidan Baack <aebaack@gmail.com>
2017-03-27 14:36:30 -0600

    Search for poems by specific poet

--- src/components/PoemList.js
+++ src/components/PoemList.js
@@ -1,5 +1,27 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 
-const PoemList = () => <p>Test</p>
+class PoemList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      poems: []
+    }
+  }
+
+  componentWillMount() {
+    axios.get(`https://poetdb.herokuapp.com/author/${this.props.match.params.poet}`)
+      .then(poems => this.setState({ poems: poems.data }))
+      .catch(err => this.setState({ poems: [] }))
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.poems.map(poem => <p>{poem.title}</p>)}
+      </div>
+    );
+  }
+}
 
 export default PoemList;
