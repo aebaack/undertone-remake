05e4ead9901c562cfb24229b71d1e1be863666c7
Aidan Baack <aebaack@gmail.com>
2017-03-29 13:27:12 -0600

    Import particles.js

--- src/components/PoemAnalysis.js
+++ src/components/PoemAnalysis.js
@@ -1,3 +1,4 @@
+import Particles from 'react-particles-js';
 import React, { Component } from 'react';
 
 import '../styles/poemanalysis.css';
@@ -9,9 +10,32 @@ export default class PoemAnalysis extends Component {
     this.state = {};
   }
 
+  componentWillMount() {
+    // setTimeout(() => document.body.classList.add('transition-background'), 200);
+    // setTimeout(() => document.body.style.backgroundColor = 'red', 2000);
+  }
+
+  componentDidMount() {
+
+  }
+
   render() {
     return (
-      <div />
-    )
+      <Particles style={{ left: 0, position: 'fixed', scale: 1.1, top: 0 }} params={{
+        particles: {
+          color: {
+            value: '#000'
+          },
+          line_linked: {
+            shadow: {
+              enable: true,
+              color: '#fff',
+              blur: 5
+            }
+          }
+        },
+        retina_detected: true
+      }}/>
+    );
   }
 }
